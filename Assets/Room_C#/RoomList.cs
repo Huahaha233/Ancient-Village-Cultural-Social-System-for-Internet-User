1c1580c173265a5a6153da2606b94c28cb1372c7
Huahaha233 <2539791707@qq.com>
2019-08-16 23:12:23 +0800

    客户端

---
+++ Assets/Room_C#/RoomList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomList : MonoBehaviour {
+    public GameObject prefab;
+    public GameObject Grid;
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < 6; i++)
+        {
+            GameObject instance = NGUITools.AddChild(Grid,prefab);
+        }
+        
+	}
+	
+}
